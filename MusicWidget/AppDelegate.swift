b5f903cf57cfcda0fc616ea507c54b8b16a3c982
Tomas Bolger <tbolger.business@gmail.com>
2022-03-27 20:22:33 +1100

    Initial Commit

---
+++ MusicWidget/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MusicWidget
+//
+//  Created by Tomas Bolger on 27/3/2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MusicWidget/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MusicWidget
+//
+//  Created by Tomas Bolger on 27/3/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
